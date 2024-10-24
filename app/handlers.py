adfa31df234a8d9cfdaa6b5980fe73cff6cfade7
misha1potapenko <misha1potapenko@yandex.ru>
2024-10-24 20:21:20 +0300

    Added class town

--- app/handlers.py
+++ app/handlers.py
@@ -1,13 +1,20 @@
 from aiogram import F, Router
 from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+import app.keyboards as kb
+from aiogram.fsm.state import StatesGroup,State
+
 
 router = Router()
 
 
+class Town(StatesGroup):
+    name_town = State
+
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет, я бот прогноза погоды. Напиши мне название города, а я скину тебе прогноз.')
+    await message.answer('Привет, я бот прогноза погоды. Пожалуйста, выбери кнопочку ниже, какой прогноз тебе нужен.',
+                         reply_markup=kb.main)
 
 
 @router.message(Command('help'))
@@ -18,3 +25,14 @@ async def get_help(message: Message):
 @router.message(F.text == 'Как дела')
 async def how_are_you(message: Message):
     await message.answer("Ok")
+
+
+@router.callback_query(F.data == 'users')
+async def for_users(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer('Напишите название города')
+
+@router.callback_query(F.data == 'professional')
+async def for_professional(callback: CallbackQuery):
+    await callback.answer()
+    await callback.message.answer('Напишите название города')
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,5 +1,10 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,
+                           InlineKeyboardMarkup, InlineKeyboardButton)
 
-main = ReplyKeyboardMarkup(keyboard=[
-    [KeyboardButton(text='Обычный прогноз погоды')]
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+
+main = InlineKeyboardMarkup(inline_keyboard=[
+    [InlineKeyboardButton(text='Обычный прогноз погоды', callback_data='users')],
+    [InlineKeyboardButton(text='Прогноз погоды для метеорологов', callback_data='professional')]
+
 ])
